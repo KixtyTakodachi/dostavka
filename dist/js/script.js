15c8e1140f65d363fcdd6314d613fa83902c24c5
Alexander <example@gmail.com>
2021-07-23 15:29:54 +0600

    Form Validation

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,4 +6,32 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arr_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arr_right.png"></button>',
     });
+    function validateForms(form){
+        $(form).validate({
+            rules:{
+                name: {
+                required: true,
+                minlength: 2
+                },
+                phone: "required",
+                email: {
+                required: true,
+                email: true
+                }
+            },
+            messages:{
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символов")
+                },
+                phone: "Пожалуйста, введите номер телефона",
+                email:{
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес потчы"
+                }
+            }
+        });
+    };
+    validateForms('#cons-form');
+    validateForms('#ques-form');
 });
