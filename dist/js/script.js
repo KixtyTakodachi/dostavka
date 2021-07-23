c3349550acdaac7aab2e1f1c8a875dc1991e3840
Alexander <example@gmail.com>
2021-07-23 17:57:52 +0600

    Fixed Form Bugs

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,20 +3,27 @@ $(document).ready(function(){
         centerMode: true,
         centerPadding: '405px',
         slidesToShow: 1,
+        infinite: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arr_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arr_right.png"></button>',
+    });
+    /* $('[data-modal=consultation]').on('click', function(){
+        $('.overlay, #thanks').fadeIn('slow')
+    }); */
+    $('.modal__close').on('click', function(){
+        $('.overlay, #thanks').fadeOut('slow')
     });
     function validateForms(form){
         $(form).validate({
             rules:{
                 name: {
-                required: true,
-                minlength: 2
+                    required: true,
+                    minlength: 2
                 },
                 phone: "required",
                 email: {
-                required: true,
-                email: true
+                    required: true,
+                    email: true
                 }
             },
             messages:{
@@ -34,4 +41,22 @@ $(document).ready(function(){
     };
     validateForms('#cons-form');
     validateForms('#ques-form');
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        let input_value = $(this).find("input").val();
+        if(input_value != ""){
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).success(function(){
+                $(this).find("input").val("");
+                $('.overlay, #thanks').fadeIn('slow');
+                $('form').trigger('reset');
+            });
+            return false
+        }
+    });
 });
