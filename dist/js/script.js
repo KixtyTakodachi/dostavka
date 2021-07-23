36b5d496ad1f238e741bba5e0e7776a81c7f24fd
Alexander <example@gmail.com>
2021-07-23 14:42:19 +0600

    Sections Finished

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel').slick({
         centerMode: true,
-        centerPadding: '60px',
+        centerPadding: '405px',
         slidesToShow: 1,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arr_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arr_right.png"></button>',
